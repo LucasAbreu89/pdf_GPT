9deafcd9c7add79583f18e70838965801c455453
LucasAbreu89 <lucasabreu@me.com>
2023-05-29 15:42:25 -0300

    questions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 from PyPDF2 import PdfReader
 import streamlit as st
 from langchain.llms import OpenAI
@@ -9,6 +10,9 @@ from langchain.chains import LLMChain
 from transformers import GPT2Tokenizer
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
 
 load_dotenv()
 
@@ -18,12 +22,20 @@ if not os.path.exists(UPLOAD_DIR):
 
 os.environ.get("OPENAI_API_KEY")
 
-st.title("🦜🔗 Summary of your PDF")
+st.title("🦜🔗 AI-powered PDF Analyzer")
+st.markdown("""
+This application uses state-of-the-art language models to summarize PDF files and answer questions about the content. 
+""")
 st.image('ai.gif')
 
-uploaded_file = st.file_uploader(
-    "Please upload your PDF file here", type=["pdf"])
+with st.sidebar:
+    st.header("Controls")
+    uploaded_file = st.file_uploader(
+        "Please upload your PDF file here", type=["pdf"])
+    query = st.text_input("Ask questions about the PDF:")
 
+if 'summary_output' not in st.session_state:
+    st.session_state['summary_output'] = None
 
 if uploaded_file is not None:
     # Save the uploaded file to the upload directory
@@ -41,43 +53,59 @@ if uploaded_file is not None:
 
     with st.expander("Extracted Text"):
         st.write(extracted_text)
-        texto = extracted_text
 
     text_splitter = RecursiveCharacterTextSplitter(
         # Set a really small chunk size, just to show.
-        chunk_size=10000,
-        chunk_overlap=20,
+        chunk_size=5000,
+        chunk_overlap=200,
         length_function=len,
     )
+
     texts = text_splitter.create_documents([extracted_text])
 
-    # st.write(texts[0])
-    # st.write(len(texts))
+    if st.session_state['summary_output'] is None:
+        if len(texts) < 4:
+            tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+            tokens = tokenizer.encode(extracted_text, add_special_tokens=False)
+            num_tokens = len(tokens)
 
-    if len(texts) < 4:
+            llm = ChatOpenAI(temperature=0)
+            map_prompt = """
+            Feel Free to use analogies
+            Write a concise summary in simple terms of the following:
+            "{text}"
+            CONCISE SUMMARY:
+            """
+            map_prompt_template = PromptTemplate(
+                template=map_prompt, input_variables=["text"])
 
-        tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-        tokens = tokenizer.encode(extracted_text, add_special_tokens=False)
-        num_tokens = len(tokens)
+            sumarry_chain = load_summarize_chain(
+                llm=llm, chain_type='map_reduce', map_prompt=map_prompt_template)
 
-        st.write("Number of tokens:", num_tokens)
+            with st.spinner('Generating Summary...'):
+                st.session_state['summary_output'] = sumarry_chain.run(texts)
 
-        llm = ChatOpenAI(temperature=0)
-        map_prompt = """
-        Feel Free to use analogies
-        Write a concise summary in simple terms of the following:
-        "{text}"
-        CONCISE SUMMARY:
-        """
-        map_prompt_template = PromptTemplate(
-            template=map_prompt, input_variables=["text"])
+    st.markdown(f"## Summary of {uploaded_file.name}:")
+    st.write(st.session_state['summary_output'])
 
-        sumarry_chain = load_summarize_chain(
-            llm=llm, chain_type='map_reduce', map_prompt=map_prompt_template)
+    # embeddings
+    chunks = text_splitter.split_text(text=extracted_text)
+    store_name = uploaded_file.name[:-4]
 
-        with st.spinner('Loading...'):
-            output = sumarry_chain.run(texts)
-        st.write(output)
-
+    if os.path.exists(f"{store_name}.pkl"):
+        with open(f"{store_name}.pkl", "rb") as f:
+            VectorStore = pickle.load(f)
     else:
-        st.write("You got a bit text")
+        with open(f"{store_name}.pkl", "wb") as f:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            pickle.dump(VectorStore, f)
+
+    if query:
+        docs = VectorStore.similarity_search(query=query, k=3)
+        llm = ChatOpenAI(temperature=0)
+        chain = load_qa_chain(llm=llm, chain_type="stuff")
+        with st.spinner('Answering your question...'):
+            response = chain.run(input_documents=docs, question=query)
+        st.markdown(f"## Answer for \"{query}\":")
+        st.write(response)
